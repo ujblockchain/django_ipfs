4c0948609fecfddcd2c60dca8f3d97a871407950
edmundrotimi <edmundrotimi@gmail.com>
2024-02-20 19:29:46 +0200

    add jinja filters

---
+++ djipfs/jinja/filters.py
@@ -0,0 +1,11 @@
+import ast
+from datetime import datetime, timedelta
+from django.utils.timezone import utc
+
+
+# init current time
+current_timestamp = datetime.now().replace(tzinfo=utc)
+
+
+def dateformat(value):
+    return f'{value.strftime("%B")} {value.strftime("%d")}, {value.strftime("%Y")}'
