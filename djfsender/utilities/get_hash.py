c760fd1bbab7b47685db9066a15876700349c40b
edmundrotimi <edmundrotimi@gmail.com>
2024-02-22 18:45:05 +0200

    add utility to get hash of object string

---
+++ djfsender/utilities/get_hash.py
@@ -0,0 +1,13 @@
+import hashlib
+
+# finding the SHA-1 message digest of a file
+# This function returns the SHA-1 hash of the file passed into it"""
+
+
+def object_hash(object_string):
+    # Calculate the hash of the read byte string
+    hash_object = hashlib.sha256(object_string)
+    hex_dig = hash_object.hexdigest()
+
+    # return the hex representation of digest
+    return hex_dig
