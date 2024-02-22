8129a04cc0ce8f6e36bf13df3f6c41f38989527b
edmundrotimi <edmundrotimi@gmail.com>
2024-02-22 18:54:09 +0200

    register model in admin

--- djfsender/admin.py
+++ djfsender/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from .models import FileSender
 
-# Register your models here.
+
+class SendFileAdmin(admin.ModelAdmin):
+    list_display = [
+        'file_id',
+        'file_name',
+        'file_hash',
+        'ipfs_hash',
+        'pin_size',
+        'pin_time_stamp',
+        'last_updated',
+    ]
+    list_display_links = [
+        'file_id',
+        'file_name',
+        'file_hash',
+        'ipfs_hash',
+        'pin_size',
+        'pin_time_stamp',
+        'last_updated',
+    ]
+    search_fields = [
+        'file_id',
+        'file_name',
+        'file_hash',
+        'ipfs_hash',
+        'pin_size',
+        'pin_time_stamp',
+        'last_updated',
+    ]
+    list_per_page = 50
+    show_full_result_count = True
+    actions_on_top = True
+    actions_on_bottom = True
+    save_as_continue = True
+    save_on_top = True
+    fields = [
+        'file_id',
+        'file_name',
+        'file_hash',
+        'ipfs_hash',
+        'pin_size',
+        'pin_time_stamp',
+        'file',
+        'file_description',
+        'last_updated',
+    ]
+
+
+# register models
+admin.site.register(FileSender, SendFileAdmin)
