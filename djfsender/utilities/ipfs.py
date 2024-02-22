884aba4d3a84e813179220d6bb2bfd38df6486c2
edmundrotimi <edmundrotimi@gmail.com>
2024-02-22 18:45:49 +0200

    add utility to fine files to ipfs using pinata

---
+++ djfsender/utilities/ipfs.py
@@ -0,0 +1,30 @@
+import requests
+from django.conf import settings
+
+url = 'https://api.pinata.cloud/pinning/pinFileToIPFS'
+
+
+# authenticate to pineta
+auth_url = 'https://api.pinata.cloud/data/testAuthentication'
+payload = {}
+headers = {'Authorization': f'Bearer {settings.PINETA_JWT}'}
+response = requests.request('GET', auth_url, headers=headers, data=payload)
+
+
+def pin_file(file_name, file_location):
+
+    # IPFS file pinning  with pineta
+    payload = {
+        'pinataOptions': '{"cidVersion": 1}',
+        'pinataMetadata': '{"name": "%s", "keyvalues": {"company": "DJIPFS"}}' % (file_name),
+    }
+
+    files = [
+        ('file', (file_name, open(file_location, 'rb'), 'application/octet-stream'))
+    ]
+
+    headers = {'Authorization': f'Bearer {settings.PINETA_JWT}'}
+
+    response = requests.request('POST', url, headers=headers, data=payload, files=files)
+
+    return response.text
