866f50b94c436056cf8f7ed92d1286d1f71e4337
edmundrotimi <edmundrotimi@gmail.com>
2024-02-22 18:46:55 +0200

    file upload validation

---
+++ djfsender/utilities/validate_files.py
@@ -0,0 +1,15 @@
+import os
+from django.core.exceptions import ValidationError
+
+# ensure file upload do not exceed 400kb
+
+
+def check_file(value):
+    #check file size
+    filesize = value.size
+
+    #ensure size does not exceed 400kb
+    if filesize > 409610 :
+        raise ValidationError('Maximum file size that can be uploaded is 400kb')
+    else:
+        return value
