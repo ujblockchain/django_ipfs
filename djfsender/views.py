be016ebc0f2a141672f23f845217a4d7ff63774e
edmundrotimi <ujsarchiblockchain@gmail.com>
2024-03-16 18:56:46 +0200

    code indent

--- djfsender/views.py
+++ djfsender/views.py
@@ -6,7 +6,6 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic.edit import CreateView
 from django.views.generic.detail import DetailView
 
-
 from .service import FileSenderService
 from .forms import FileSenderForm
 from .models import FileSender
@@ -30,48 +29,45 @@ class HomeView(SuccessMessageMixin, CreateView):
                 # check if the file hash already exits
                 # init file hash using incoming byte string
                 file_stream_hash = FileSenderService.get_object_hash(formfile.read())
-                check_if_hash_exist = FileSenderService.check_hash_exist(
-                    file_stream_hash
-                )
+                check_if_hash_exist = FileSenderService.check_hash_exist(file_stream_hash)
 
-        # only save file if hash does not exist
-        if not check_if_hash_exist:
-            # set file name
-            file_name = FileSenderService.get_file_name()
-            # set file extension
-            # this is needed to make the file extension of saved file dynamic
-            # based on the uploaded file
-            uploaded_file_extension = formfile.content_type.split('/')
-            file_extension = uploaded_file_extension[len(uploaded_file_extension) - 1]
+                # only save file if hash does not exist
+                if not check_if_hash_exist:
+                    # set file name
+                    file_name = FileSenderService.get_file_name()
+                    # set file extension
+                    # this is needed to make the file extension of saved file dynamic
+                    # based on the uploaded file
+                    uploaded_file_extension = formfile.content_type.split('/')
+                    file_extension = uploaded_file_extension[len(uploaded_file_extension) - 1]
 
-            # save file to media root
-            FileSenderService.upload_to_media_root(formfile, file_name, file_extension)
+                    # save file to media root
+                    FileSenderService.upload_to_media_root(formfile, file_name, file_extension)
 
-            # set file path
-            file_path = f'{settings.MEDIA_ROOT}/{file_name}.{file_extension}'
-            # Upload file to ipfs
-            file = FileSenderService.ipfs_pin_file(file_name, file_path)
-            # convert json to dictionary
-            file_dict_response = json.loads(file)
+                    # set file path
+                    file_path = f'{settings.MEDIA_ROOT}/{file_name}.{file_extension}'
+                    # Upload file to ipfs
+                    file = FileSenderService.ipfs_pin_file(file_name, file_path)
+                    # convert json to dictionary
+                    file_dict_response = json.loads(file)
 
-            # save model
-            FileSenderService.create_file_sender(
-                file_name,
-                file_path,
-                file_stream_hash,
-                file_dict_response['IpfsHash'],
-                file_dict_response['PinSize'],
-                form.cleaned_data['file_description'],
-                file_dict_response['Timestamp'],
-            )
+                    # save model
+                    FileSenderService.create_file_sender(
+                        file_name,
+                        file_path,
+                        file_stream_hash,
+                        file_dict_response['IpfsHash'],
+                        file_dict_response['PinSize'],
+                        form.cleaned_data['file_description'],
+                        file_dict_response['Timestamp'],
+                    )
 
         # redirect to view page
         return HttpResponsePermanentRedirect(
             reverse(
                 'file_details',
                 kwargs={'file_id': FileSenderService.get_file_id(file_stream_hash)},
-            )
-        )
+            ))
 
     # if form is invalid, return it
     def form_invalid(self, form):
